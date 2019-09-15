acf392715f3cfb4f5adb80f956f247ebe60f3a1d
Naoki Egawa <nao.respawn@gmail.com>
2019-09-15 12:28:05 +0900

    Create ShowPage

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  get 'posts/index'
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
