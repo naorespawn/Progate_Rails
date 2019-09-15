f748aa414624d4b7e60fb7f54e578fcd2c0c221f
Naoki Egawa <nao.respawn@gmail.com>
2019-09-15 12:55:44 +0900

    PostsViewPage Created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'posts/index'
+  get 'posts/:id' => 'posts#show'
   get '/' => 'home#top'
   get 'about' => 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
